8dbe741196d39754840d8e1ce67cf937ec0ed64a
Guillaume Roullet <guillaume.roullet@univ-brest.fr>
2022-11-07 17:43:41 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# The rhombic triacontahedron
+
+The goal of this project is to craft, with thick paper a [rhombic
+triacontahedron](https://en.wikipedia.org/wiki/Rhombic_triacontahedron),
+hereafter shorten to RTCH. And because we are doing oceanography, we
+want this RTCH to represent a meaningful quantity, for instance the
+ocean bathymetry.
+
+<img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/Rhombictriacontahedron.jpg" width="400">
+
+
+## Who?
+With the M1 Students of the Marine Physics master's program!
+
+## What will we learn?
+We'll learn about spherical geometry and projections. We'll improve
+our programming skills because we'll do that in Python.We will also
+improve the group work techniques because this is a collaborative
+project.
+
---
+++ main.py
@@ -0,0 +1,18 @@
+
+def project_outline():
+    s = ["Here are the following tasks I see"]
+    s += ["   (inspired from how people do with the sperical cube)"]
+    s += [""]
+    s += [" 1) project a field on a rhombus drawn at the surface of the sphere"]
+    s += [" 2) rotate a rhombus centered at the North pole to any location"]
+    s += [" 3) determine the coordinates and the orientation of the 30 faces"]
+    s += [" 4) assemble the rhombuses in blocks to form the pattern."]
+    s += [""]
+    s += ["Task 4 is optionnal. We can either print the 30 faces detached from"]
+    s += ["each other, or having them grouped by blocks to spare some pasting"]
+    s += ["during the crafting"]
+    print("\n".join(s))
+
+
+if __name__ == "__main__":
+    project_outline()
