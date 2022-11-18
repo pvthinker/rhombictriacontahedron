9ca64b8482faa67abbb17fff71bc68c9e8493179
pvthinker <roullet@univ-brest.fr>
2022-11-18 14:49:09 +0100

    Introduce the quaternions

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 The goal of this project is to craft, with thick paper a [rhombic
 triacontahedron](https://en.wikipedia.org/wiki/Rhombic_triacontahedron),
-hereafter shorten to RTCH. And because we are doing oceanography, we
-want this RTCH to represent a meaningful quantity, for instance the
+hereafter shorten to RT. And because we are doing oceanography, we
+want this RT to represent a meaningful quantity, for instance the
 ocean bathymetry.
 
 <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/Rhombictriacontahedron.jpg" width="400">
@@ -18,3 +18,12 @@ our programming skills because we'll do that in Python.We will also
 improve the group work techniques because this is a collaborative
 project.
 
+## Spherical rotations
+For spherical rotations, we have two options: i) the matrix route or
+the ii) the quaternions one. Guess which one we'll use ?
+
+Don't know what the quaternions are ? They are like the complex
+numbers but with two additionnal imaginary numbers: so instead of 1
+and i, there is 1, i, j and k. To get the connection with 3D rotations
+watch [this video](https://www.youtube.com/watch?v=zjMuIxRvygQ) by the
+amazing Grant Sanderson, aka 3blue1brown.
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 
 def project_outline():
-    s = ["Here are the following tasks I see"]
-    s += ["   (inspired from how people do with the sperical cube)"]
-    s += [""]
-    s += [" 1) project a field on a rhombus drawn at the surface of the sphere"]
-    s += [" 2) rotate a rhombus centered at the North pole to any location"]
-    s += [" 3) determine the coordinates and the orientation of the 30 faces"]
-    s += [" 4) assemble the rhombuses in blocks to form the pattern."]
-    s += [""]
-    s += ["Task 4 is optionnal. We can either print the 30 faces detached from"]
-    s += ["each other, or having them grouped by blocks to spare some pasting"]
-    s += ["during the crafting"]
-    print("\n".join(s))
+    """
+    Here are the following tasks I see
+    (inspired from how people do with the spherical cube)
+
+    1) project a field on a rhombus drawn at the surface of the sphere
+    2) rotate a rhombus centered at the North pole to any location
+    3) determine the coordinates and the orientation of the 30 faces
+    4) assemble the rhombuses in blocks to form the set.
+
+    Task 4 is optionnal. We can either print the 30 faces detached from
+    each other, or having them grouped by blocks to spare some pasting
+    during the crafting
+    """
+    pass
 
 
 if __name__ == "__main__":
-    project_outline()
+    print(project_outline.__doc__)
